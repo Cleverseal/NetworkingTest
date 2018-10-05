0aea79535ac6f7c878fd369c4d31670eed1250b7
Mykyta Tilinin <nikita.tilinin@nixsolutions.com>
2018-10-05 18:42:47 +0300

    Added Networking player move

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,8 +1,9 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Networking;
 
-public class PlayerMovement : MonoBehaviour {
+public class PlayerMovement : NetworkBehaviour {
     float moveX, moveY;
     Rigidbody2D rb;
 	// Use this for initialization
@@ -12,10 +13,16 @@ public class PlayerMovement : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+        if (!isLocalPlayer) return;
         moveX = Input.GetAxis("Horizontal");
         moveY = Input.GetAxis("Vertical");
 
         rb.MovePosition((Vector2)transform.position + new Vector2(moveX, moveY) * 1);
 
 	}
+
+    public override void OnStartLocalPlayer()
+    {
+        GetComponent<MeshRenderer>().material.color = Color.red;
+    }
 }
