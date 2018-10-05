bad8241b1197c90778e6a8e63f84cdfd29a5d8e8
Mykyta Tilinin <nikita.tilinin@nixsolutions.com>
2018-10-05 17:42:16 +0300

    initial commit

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+    float moveX, moveY;
+    Rigidbody2D rb;
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        moveX = Input.GetAxis("Horizontal");
+        moveY = Input.GetAxis("Vertical");
+
+        rb.MovePosition((Vector2)transform.position + new Vector2(moveX, moveY) * 1);
+
+	}
+}
